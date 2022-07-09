14d7e54e44eed5cd6e4f1203005e1ba8f2379c18
unknown <jessespeks@hotmail.com>
2022-07-09 11:55:01 -0400

    added notes

--- fetch.js
+++ fetch.js
@@ -9,3 +9,16 @@ const result = getRequest(url);
 // should return a promise when using a 'fetch' function on a url
 
 //JSON always in caps
+
+// example
+async function getPets(){
+    let response = await response.json()
+    let result = await response.json()
+    console.log(result)
+}
+
+// •GET: Retrieves data
+// •POST: Adds new data
+// •PATCH: Updates data
+// •PUT: Replaces data
+// •DELETE: Removes data
